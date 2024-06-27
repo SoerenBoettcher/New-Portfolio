209f27a5dc5157cd4657cbf994295f8c092d2482
Sören <soeren.boettcher@dci-student.org>
2024-06-27 11:18:50 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,16 +10,23 @@ const Navbar = () => {
     { name: "PROJECTS", link: "#projects" },
     { name: "CONTACT", link: "#contact" },
   ];
+
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      const nav = document.querySelector("nav");
-      window.scrollY > 0 ? setSticky(true) : setSticky(false);
-    });
+    const handleScroll = () => {
+      setSticky(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
+
   return (
     <nav
       className={`fixed w-full left-0 top-0 z-[999] ${
-        sticky ? "bg-white/80  text-gray-900" : "text-white"
+        sticky ? "bg-white/80 text-gray-900" : "text-white"
       }`}
     >
       <div className="flex items-center justify-between">
@@ -30,40 +37,39 @@ const Navbar = () => {
           </h4>
         </div>
         <div
-          className={` ${
+          className={`${
             sticky ? "md:bg-white/0 bg-white" : "bg-white"
-          } text-gray-900 md:block hidden px-7 py-2 font-medium  rounded-bl-full`}
+          } text-gray-900 md:block hidden px-7 py-2 font-medium rounded-bl-full`}
         >
           <ul className="flex items-center gap-1 py-2 text-lg">
-            {menuLinks?.map((menu, i) => (
+            {menuLinks.map((menu, i) => (
               <li key={i} className="px-6 hover:text-cyan-600">
-                <a href={menu?.link}>{menu?.name}</a>
+                <a href={menu.link}>{menu.name}</a>
               </li>
             ))}
           </ul>
         </div>
         <div
           onClick={() => setOpen(!open)}
-          className={`z-[999]  ${
-            open ? "text-gray-900" : "text-gray-100"
+          className={`z-[999] ${
+            open ? "text-gray-900" : "text-cyan-600"
           } text-3xl md:hidden m-5`}
         >
           <ion-icon name="menu"></ion-icon>
         </div>
         <div
-          className={`md:hidden text-gray-900 absolute w-2/3 h-screen
-      px-7 py-2 font-medium bg-white top-0 duration-300 ${
-        open ? "right-0" : "right-[-100%]"
-      }`}
+          className={`md:hidden text-gray-900 absolute w-2/3 h-screen px-7 py-2 font-medium bg-white top-0 duration-300 ${
+            open ? "right-0" : "right-[-100%]"
+          }`}
         >
           <ul className="flex flex-col justify-center h-full gap-10 py-2 text-lg">
-            {menuLinks?.map((menu, i) => (
+            {menuLinks.map((menu, i) => (
               <li
                 onClick={() => setOpen(false)}
                 key={i}
                 className="px-6 hover:text-cyan-600"
               >
-                <a href={menu?.link}>{menu?.name}</a>
+                <a href={menu.link}>{menu.name}</a>
               </li>
             ))}
           </ul>
