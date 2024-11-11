7c30483be79fb7c8a3f6de84792bc9c8f2b6c35c
Sören <soeren.boettcher@dci-student.org>
2024-11-11 10:38:44 +0100

    refresh

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,54 +2,54 @@ import { useRef } from "react";
 import my from "../assets/my1.jpeg";
 import Type from "./Type";
 
-const socialMediaLinks = ["https://github.com/SoerenBoettcher"];
+const socialMediaLinks = [
+    "https://github.com/SoerenBoettcher",
+    "https://www.linkedin.com/in/s%C3%B6ren-b%C3%B6ttcher-406abb318/",
+];
 
 const Hero = () => {
-  const contactRef = useRef(null);
+    const contactRef = useRef(null);
 
-  const social_media = ["logo-github"];
-  return (
-    <section
-      id="home"
-      className="min-h-screen flex py-20 md:flex-row flex-col items-center"
-    >
-      <div className="flex-1 flex items-center justify-center h-full">
-        <img
-          src={my}
-          alt="Foto von Sören"
-          className="md:w-2/3 h-[700px] mb-6 rounded-xl border-2 border-gray-600 object-cover"
-        />
-      </div>
-      <div className="flex-1">
-        <div className="md:text-left text-center">
-          <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
-            <span className="text-cyan-600 md:text-6xl text-5xl">
-              Hello!
-              <br />
-            </span>
-            My Name is <span>Sören Böttcher</span>
-          </h1>
-          <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600">
-            <Type />
-          </h4>
-          <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
-            {social_media?.map((icon, index) => (
-              <a
-                key={icon}
-                href={socialMediaLinks[index]}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-gray-600 hover:text-white cursor-pointer "
-              >
-                <ion-icon name={icon}></ion-icon>
-              </a>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div ref={contactRef}></div>
-    </section>
-  );
+    const social_media = ["logo-github", "logo-linkedin"];
+    return (
+        <section id="home" className="min-h-screen flex py-20 md:flex-row flex-col items-center">
+            <div className="flex-1 flex items-center justify-center h-full">
+                <img
+                    src={my}
+                    alt="Foto von Sören"
+                    className="md:w-2/3 h-[700px] mb-6 rounded-xl border-2 border-gray-600 object-cover"
+                />
+            </div>
+            <div className="flex-1">
+                <div className="md:text-left text-center">
+                    <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
+                        <span className="text-cyan-600 md:text-6xl text-5xl">
+                            Hello!
+                            <br />
+                        </span>
+                        My Name is <span>Sören Böttcher</span>
+                    </h1>
+                    <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-600">
+                        <Type />
+                    </h4>
+                    <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
+                        {social_media?.map((icon, index) => (
+                            <a
+                                key={icon}
+                                href={socialMediaLinks[index]}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-gray-600 hover:text-white cursor-pointer "
+                            >
+                                <ion-icon name={icon}></ion-icon>
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div ref={contactRef}></div>
+        </section>
+    );
 };
 
 export default Hero;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,79 +1,85 @@
 const Skills = () => {
-  const skills = [
-    {
-      name: "HTML",
-      logo: "logo-html5",
-      level: "Intermediate",
-      count: 80,
-    },
-    {
-      name: "CSS",
-      logo: "logo-css3",
-      level: "Intermediate",
-      count: 75,
-    },
-    {
-      name: "SASS",
-      logo: "logo-sass",
-      level: "Beginner",
-      count: 35,
-    },
-    {
-      name: "JAVASCRIPT",
-      logo: "logo-javascript",
-      level: "Beginner",
-      count: 60,
-    },
-    {
-      name: "REACT",
-      logo: "logo-react",
-      level: "Beginner",
-      count: 50,
-    },
-    {
-      name: "NPM",
-      logo: "logo-npm",
-      level: "Beginner",
-      count: 35,
-    },
-    {
-      name: "FIREBASE",
-      logo: "logo-firebase",
-      level: "Beginner",
-      count: 40,
-    },
-  ];
-  return (
-    <section id="skills" className="py-10 bg-gray-800 relative">
-      <div className="mt-8 text-gray-100 text-center">
-        <h3 className="text-4xl font-semibold">
-          My <span className="text-cyan-600">Skills</span>
-        </h3>
-        <p className="text-gray-400 mt-3 text-lg">My knowledge</p>
-        <div className="flex items-center justify-center mt-12 gap-8 flex-wrap">
-          {skills?.map((skill, i) => (
-            <div
-              key={i}
-              className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-4 mb-10 rounded-xl"
-            >
-              <p className="text-xl mb-2">{skill.name}</p>
-              <div
-                style={{
-                  background: `conic-gradient(rgb(8,145,170) ${skill.count}%,#ddd ${skill.count}%)`,
-                }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
-              >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
-                  <ion-icon name={skill.logo}> </ion-icon>
+    const skills = [
+        {
+            name: "HTML",
+            logo: "logo-html5",
+            level: "Experienced",
+            count: 85,
+        },
+        {
+            name: "CSS",
+            logo: "logo-css3",
+            level: "Effective",
+            count: 75,
+        },
+        {
+            name: "SASS",
+            logo: "logo-sass",
+            level: "Effective",
+            count: 55,
+        },
+        {
+            name: "JAVASCRIPT",
+            logo: "logo-javascript",
+            level: "Experienced",
+            count: 80,
+        },
+        {
+            name: "NODEJS",
+            logo: "logo-nodejs",
+            level: "Experienced",
+            count: 85,
+        },
+        {
+            name: "REACT",
+            logo: "logo-react",
+            level: "Experienced",
+            count: 80,
+        },
+        {
+            name: "NPM",
+            logo: "logo-npm",
+            level: "Experienced",
+            count: 75,
+        },
+        {
+            name: "FIREBASE",
+            logo: "logo-firebase",
+            level: "Effective",
+            count: 44,
+        },
+    ];
+    return (
+        <section id="skills" className="py-10 bg-gray-800 relative">
+            <div className="mt-8 text-gray-100 text-center">
+                <h3 className="text-4xl font-semibold">
+                    My <span className="text-cyan-600">Skills</span>
+                </h3>
+                <p className="text-gray-400 mt-3 text-lg">My knowledge</p>
+                <div className="flex items-center justify-center mt-12 gap-8 flex-wrap">
+                    {skills?.map((skill, i) => (
+                        <div
+                            key={i}
+                            className="border-2 group border-cyan-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-4 mb-10 rounded-xl"
+                        >
+                            <p className="text-xl mb-2">{skill.name}</p>
+                            <div
+                                style={{
+                                    background: `conic-gradient(rgb(8,145,170) ${skill.count}%,#ddd ${skill.count}%)`,
+                                }}
+                                className="w-32 h-32 flex items-center justify-center rounded-full"
+                            >
+                                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-cyan-600">
+                                    <ion-icon name={skill.logo}> </ion-icon>
+                                </div>
+                            </div>
+                            <p className="text-xl mt-3">{skill.level}</p>
+                        </div>
+                    ))}
                 </div>
-              </div>
-              <p className="text-xl mt-3">{skill.level}</p>
             </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Skills;
